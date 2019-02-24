9ceffd13e290bbbbae08b40605c5b65a8be8c5ad
Tom <25043847+Douile@users.noreply.github.com>
2019-02-24 20:57:13 +0000

    LICENSE

--- int_to_bits.js
+++ int_to_bits.js
@@ -1,8 +1,25 @@
+/*
+int_to_bits.js
+Copyright (C) 2019 Douile
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
 function bits(n) {
     var o = '';
     var x = 0;
     while (true) {
-        o = ((n & (1 << x)) > 0 ? '1' : '0') + o; 
+        o = ((n & (1 << x)) > 0 ? '1' : '0') + o;
         x++;
         if (n - 2**x < 0) break;
     }
--- table_to_array.js
+++ table_to_array.js
@@ -1,3 +1,20 @@
+/*
+table_to_array.js
+Copyright (C) 2019 Douile
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
 function* items(array) {
   for (var i=0;i<array.length;i++) {
     yield array[i];
